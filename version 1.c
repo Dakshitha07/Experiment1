ea23e01fe56dcd445889ac66f1afee5e097c4a6e
Dakshitha07 <128288058+Dakshitha07@users.noreply.github.com>
2025-01-04 12:02:34 +0530

    version 1.c

---
+++ version 1.c
@@ -0,0 +1,13 @@
+#inlcude<stdio.h>
+#include <stdio.h>
+
+int main() {
+    int a = 11, b = 22, c = 36, x = 4;
+    int y;
+
+    y = a * x * x + b * x + c;
+
+    printf("The predicted value is %d units\n", y);
+
+    return 0;
+}
