76429cd4185dfc87c15573d19027c0b8ccc9c020
Dakshitha <128288058+Dakshitha07@users.noreply.github.com>
2025-03-20 12:29:21 +0530

    version 1.c

--- version 1.c
+++ version 1.c
@@ -1,7 +1,7 @@
 #inlcude<stdio.h>
 #include <stdio.h>
 int main() {
-    int a = 11, b = 26, c = 46, x = 5;
+    int a = 31, b = 26, c = 46, x = 5;
     int y;
     y = a * x * x + b * x + c;
     printf("The predicted value is %d units\n", y);
